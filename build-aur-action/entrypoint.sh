8b2c821983316b295b737935b29f9b74f9a55778
Celestial.y <celestial.y@outlook.com>
2025-10-23 22:50:19 +0800

    Using pkgbase

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -14,12 +14,30 @@ EOM
 pacman-key --init
 pacman-key --lsign-key "farseerfc@archlinux.org"
 pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
-pacman -Syu --noconfirm archlinux-keyring
-pacman -Syu --noconfirm yay
+pacman -Su --noconfirm archlinux-keyring
+pacman -Su --noconfirm yay jq
 if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
     pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
 fi
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
-cd "./$pkgname" || exit 1
+
+# Find the actual build directory (pkgbase) created by yay.
+# Some AUR packages use a different pkgbase directory name,
+# e.g. otf-space-grotesk has a pkgbase 38c3-styles, 
+# when using yay -S otf-space-grotesk, it's built under folder 38c3-styles.
+function get_pkgbase(){
+  local pkg="$1"   # e.g. otf-space-grotesk
+  url="https://aur.archlinux.org/rpc/?v=5&type=info&arg=${pkg}"
+  resp="$(curl -sS "$url")"
+  pkgbase="$(printf '%s' "$resp" | jq -r '.results[0].PackageBase // .results[0].Name')"
+  if [ -z "$pkgbase" ] || [ "$pkgbase" = "null" ]; then
+    echo "Package not found in AUR: $pkg" >&2
+    exit 1
+  fi
+  echo "$pkgbase"
+}
+
+pkgdir=$(get_pkgbase $pkgname)
+cd "$pkgdir" || exit 1
 python3 ../build-aur-action/encode_name.py
